4a5bcc36b847e44600fa84dcd4fcf303ab2916ff
Jordan <camiloarmijosantifonamusic@gmail.com>
2021-12-14 17:51:10 -0500

    Store data in array for projects content

--- main.js
+++ main.js
@@ -22,3 +22,46 @@ closeIcon.addEventListener('click', disapear);
 links.forEach((link) => {
   link.addEventListener('click', disapear);
 });
+
+// Popup Detail Window
+
+const popupContent = [
+  {
+    titleProject: 'Tonic',
+    features: ['canopy', 'Back End Dev', '2015'],
+    imgProject: 'images/cardsWorksSection/card1Detail.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    links: ['https://jord4n-ac.github.io/my-portfolio/#section-2',
+    'https://github.com/Jord4N-AC/my-portfolio']
+  },
+  {
+    titleProject: 'Multi-Post Stories',
+    features: ['facebook', 'Full Stack Dev', '2015'],
+    imgProject: 'images/cardsWorksSection/card2.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    links: ['https://jord4n-ac.github.io/my-portfolio/#section-2',
+    'https://github.com/Jord4N-AC/my-portfolio']
+  },
+  {
+    titleProject: 'Facebook 360',
+    features: ['canopy', 'Full Stack Dev', '2015'],
+    imgProject: 'images/cardsWorksSection/card1Detail.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    links: ['https://jord4n-ac.github.io/my-portfolio/#section-2',
+    'https://github.com/Jord4N-AC/my-portfolio']
+  },
+  {
+    titleProject: 'Uber Navigation',
+    features: ['Uber', 'Lead Developer', '2018'],
+    imgProject: 'images/cardsWorksSection/card1Detail.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    links: ['https://jord4n-ac.github.io/my-portfolio/#section-2',
+    'https://github.com/Jord4N-AC/my-portfolio']
+  }
+
+  //name, description, featured image, technologies, link to live version, link to source.
+];
