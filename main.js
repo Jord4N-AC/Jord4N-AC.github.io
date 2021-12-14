1887dc58a66bb61e9b43fef5650149989eaa4da0
Bondok6 <kirolousbondok6@gmail.com>
2021-12-14 17:42:30 +0200

    Update main.js file
    Add links functionality

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector("#top-menu");
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('#top-menu');
+const closeIcon = document.querySelector('.close-icon');
+const links = document.querySelectorAll('#top-menu ul li');
 
 function display() {
-  mobileMenu.style.transform = "translateX(0%)";
+  mobileMenu.style.transform = 'translateX(0%)';
 }
 
-menuIcon.addEventListener("click", display);
+function disapear() {
+  mobileMenu.style.transform = 'translateX(100%)';
+}
+
+menuIcon.addEventListener('click', display);
+closeIcon.addEventListener('click', disapear);
+
+links.forEach((link) => {
+  link.addEventListener('click', disapear);
+});
