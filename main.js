32362e3be5c833e4ce3c7f0c54ed16fcc79d2cdb
Jordan <camiloarmijosantifonamusic@gmail.com>
2021-12-14 11:35:16 -0500

    Update style and functionality on mobile menu

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ const menuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('#top-menu');
 const closeIcon = document.querySelector('.close-icon');
 const links = document.querySelectorAll('#top-menu ul li');
+const blurIntro = document.querySelector('#intro');
 
 function display() {
   mobileMenu.style.transform = 'translateX(0%)';
+  blurIntro.style.filter = 'blur(3px)';
+  menuIcon.style.display = 'none';
 }
 
 function disapear() {
-  mobileMenu.style.transform = 'translateX(100%)';
+  mobileMenu.style.transform = 'translateX(-100%)';
+  blurIntro.style.filter = 'blur(0px)';
+  menuIcon.style.display = 'inline-block';
 }
 
 menuIcon.addEventListener('click', display);
