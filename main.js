5cfec9be1e2c8c9107a7ea065504742253baf5c5
Jordan <camiloarmijosantifonamusic@gmail.com>
2021-12-16 18:48:48 -0500

    Update showModal function

--- main.js
+++ main.js
@@ -1,153 +1,162 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector("#top-menu");
-const closeIcon = document.querySelector(".close-icon");
-const links = document.querySelectorAll("#top-menu ul li");
-const blurIntro = document.querySelector("#intro");
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('#top-menu');
+const closeIcon = document.querySelector('.close-icon');
+const links = document.querySelectorAll('#top-menu ul li');
+const blurIntro = document.querySelector('#intro');
 
 function display() {
-  mobileMenu.style.transform = "translateX(0%)";
-  blurIntro.style.filter = "blur(3px)";
-  menuIcon.style.display = "none";
+  mobileMenu.style.transform = 'translateX(0%)';
+  blurIntro.style.filter = 'blur(3px)';
+  menuIcon.style.display = 'none';
 }
 
 function disapear() {
-  mobileMenu.style.transform = "translateX(-100%)";
-  blurIntro.style.filter = "blur(0px)";
-  menuIcon.style.display = "inline-block";
+  mobileMenu.style.transform = 'translateX(-100%)';
+  blurIntro.style.filter = 'blur(0px)';
+  menuIcon.style.display = 'inline-block';
 }
 
-menuIcon.addEventListener("click", display);
-closeIcon.addEventListener("click", disapear);
+menuIcon.addEventListener('click', display);
+closeIcon.addEventListener('click', disapear);
 
 links.forEach((link) => {
-  link.addEventListener("click", disapear);
+  link.addEventListener('click', disapear);
 });
 
 // Popup Detail Window
-
 const popupContent = [
   {
-    titleProject: "Tonic",
-
-    features: ["canopy", "Back End Dev", "2015"],
-
-    imgProject: "images/cardsWorksSection/card1Detail.svg",
-
-    contentProject: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-
-    technologies: ["html", "Ruby on rails", "css", "javaScript"],
-
+    titleProject: 'Tonic',
+    features: ['canopy', 'BackEndDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card1Detail.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio'
     ],
+    icons: [
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg'
+    ]
   },
   {
-    titleProject: "Multi-Post Stories",
-    features: ["facebook", "Full Stack Dev", "2015"],
-    imgProject: "images/cardsWorksSection/card2.svg",
-    contentProject: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    titleProject: 'Multi-Post Stories',
+    features: ['facebook', 'FullStackDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card2.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
 
-    technologies: ["html", "Ruby on rails", "css", "javaScript"],
-
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio'
     ],
+    icons: [
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg'
+    ]
   },
   {
-    titleProject: "Facebook 360",
-    features: ["canopy", "Full Stack Dev", "2015"],
-    imgProject: "images/cardsWorksSection/card1Detail.svg",
-    contentProject: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    titleProject: 'Facebook 360',
+    features: ['canopy', 'FullStackDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card3.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
 
-    technologies: ["html", "Ruby on rails", "css", "javaScript"],
-
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio'
     ],
+    icons: [
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg'
+    ]
   },
   {
-    titleProject: "Uber Navigation",
-    features: ["Uber", "Lead Developer", "2018"],
-    imgProject: "images/cardsWorksSection/card1Detail.svg",
-    contentProject: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    titleProject: 'Uber Navigation',
+    features: ['Uber', 'LeadDeveloper', '2018'],
+    imgProject: 'images/cardsWorksSection/card4.svg',
+    contentProject: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
 
-    technologies: ["html", "Ruby on rails", "css", "javaScript"],
-
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio'
     ],
+    icons: [
+      'images/icons/exit-icon-gray.svg',
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg'
+    ]
   },
-
-  //name, description, featured image, technologies, link to live version, link to source.
 ];
 
 // Form Validation
 
 function showModal(ind) {
-  const section = document.querySelector('#section-2');
+  const section = document.querySelectorAll(".card");
 
   const modalHtml = `
-  <div class="modal-container">
-  <div class="header-container">
-      <div>
-          <h3 class="proj-header">Tonic</h3>
-          <img class="x-icon" src="images/icons/exit-icon-gray.svg" alt="close window icon">
-      </div>
-      <ul class="features">
-          <li class="f f1 upper-case">canopy</li>
-          <li class="dot">.</li>
-          <li class="f f2">BackEndDev</li>
-          <li class="dot">.</li>
-          <li class="f f3">2015</li>
-      </ul>
-  </div>
-  <div class="img-container">
-      <img class="img-proj" src="${popupContent[ind].imgProject}" alt="project1 image modal">
-  </div>
-  <div class="content-container">
-      <p class="p p-proj">
-          A daily selection of privately personalized reads; no accounts or sign-ups required.
-      </p>
-      <div class="tech-btns-container">
-          <ul class="tags">
-              <li>html</li>
-              <li>css</li>
-              <li>javaScript</li>
-              <li class="hide">github</li>
-              <li class="hide">ruby</li>
-              <li class="hide">Bootstraps</li>
-          </ul>
-          <div class="btns-container">
-              <a class="btn sp-button">
-                  See live
-                  <img src="images/cardsWorksSection/see_live_btn.svg" alt="See live icon">
-              </a>
-              <a class="btn sp-button">
-                  See Source
-                  <img src="images/cardsWorksSection/github_btn.svg" alt="Github icon">
-              </a>
+      <div class="modal-container">
+          <div class="header-container">
+              <div>
+                  <h3 class="proj-header">${popupContent[ind].titleProject}</h3>
+                  <img class="x-icon" src="${popupContent[ind].icons[0]}" alt="close window icon">
+              </div>
+              <ul class="features">
+                  <li class="f f1 upper-case">${popupContent[ind].features[0]}</li>
+                  <li class="dot">.</li>
+                  <li class="f f2">${popupContent[ind].features[1]}</li>
+                  <li class="dot">.</li>
+                  <li class="f f3">${popupContent[ind].features[2]}</li>
+              </ul>
+          </div>
+          <div class="img-container">
+              <img class="img-proj" src="${popupContent[ind].imgProject}" alt="project1 image modal">
+          </div>
+          <div class="content-container">
+              <p class="p p-proj">
+                  ${popupContent[ind].contentProject}
+              </p>
+              <div class="tech-btns-container">
+                  <ul class="tags">
+                      <li>${popupContent[ind].technologies[0]}</li>
+                      <li>${popupContent[ind].technologies[1]}</li>
+                      <li>${popupContent[ind].technologies[2]}</li>
+                      <li class="hide">${popupContent[ind].technologies[3]}</li>
+                      <li class="hide">${popupContent[ind].technologies[4]}</li>
+                      <li class="hide">${popupContent[ind].technologies[5]}</li>
+                  </ul>
+                  <div class="btns-container">
+                      <a class="btn sp-button" href="${popupContent[ind].links[0]}">
+                          See live
+                          <img src="${popupContent[ind].icons[1]}" alt="See live icon">
+                      </a>
+                      <a class="btn sp-button" href="${popupContent[ind].links[1]}">
+                          See Source
+                          <img src="${popupContent[ind].icons[2]}" alt="Github icon">
+                      </a>
+                  </div>
+              </div>
           </div>
       </div>
-  </div>
-</div>
-`;
-
-  section.insertAdjacentHTML('afterbegin', modalHtml);
+  `;
+  
+  section[ind].insertAdjacentHTML('afterbegin', modalHtml);  
 
   const closeModalIcon = document.querySelector('.x-icon');
   const modal = document.querySelector('.modal-container');
 
   closeModalIcon.addEventListener('click', () => {
-    modal.remove()
+    modal.remove();
   });
 }
 
 document.querySelectorAll('.sp-button').forEach((btn, ind) => {
-  btn.addEventListener('click', () => {
+  btn.addEventListener('click', () => {    
     showModal(ind);
   });
 });
