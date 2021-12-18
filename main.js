7275455ffbff385555ea20e64f3480a0dc2b38a2
Jordan <camiloarmijosantifonamusic@gmail.com>
2021-12-18 00:45:40 -0500

    Fix linters errors

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector("#top-menu");
-const closeIcon = document.querySelector(".close-icon");
-const links = document.querySelectorAll("#top-menu ul li");
-const blurIntro = document.querySelector("#intro");
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('#top-menu');
+const closeIcon = document.querySelector('.close-icon');
+const links = document.querySelectorAll('#top-menu ul li');
+const blurIntro = document.querySelector('#intro');
 
 function display() {
-  mobileMenu.style.transform = "translateX(0%)";
-  blurIntro.style.filter = "blur(3px)";
-  menuIcon.style.display = "none";
+  mobileMenu.style.transform = 'translateX(0%)';
+  blurIntro.style.filter = 'blur(3px)';
+  menuIcon.style.display = 'none';
 }
 
 function disapear() {
-  mobileMenu.style.transform = "translateX(-100%)";
-  blurIntro.style.filter = "blur(0px)";
-  menuIcon.style.display = "inline-block";
+  mobileMenu.style.transform = 'translateX(-100%)';
+  blurIntro.style.filter = 'blur(0px)';
+  menuIcon.style.display = 'inline-block';
 }
 
-menuIcon.addEventListener("click", display);
-closeIcon.addEventListener("click", disapear);
+menuIcon.addEventListener('click', display);
+closeIcon.addEventListener('click', disapear);
 
 links.forEach((link) => {
-  link.addEventListener("click", disapear);
+  link.addEventListener('click', disapear);
 });
 
 // Popup Detail Window and Section 2 Content
@@ -34,13 +34,13 @@ const popupContent = [
         'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
     technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg", // x-Icon
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg",
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
       'See Project',
@@ -57,13 +57,13 @@ const popupContent = [
         'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
     technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg", // x-Icon
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg",
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
       'See Project',
@@ -78,8 +78,8 @@ const popupContent = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
     technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
       'images/icons/exit-icon-gray.svg',
@@ -91,21 +91,21 @@ const popupContent = [
     ],
   },
   {
-    titleProject: "Uber Navigation",
-    features: ["Uber", "LeadDeveloper", "2018"],
-    imgProject: "images/cardsWorksSection/card4.svg",
+    titleProject: 'Uber Navigation',
+    features: ['Uber', 'LeadDeveloper', '2018'],
+    imgProject: 'images/cardsWorksSection/card4.svg',
     contentProject:
     ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.'],      
-    technologies: ["html", "css", "javaScript", "github", "ruby", "Bootstraps"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio",
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg",
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg",
+      'images/icons/exit-icon-gray.svg',
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
       'See Project',
@@ -116,7 +116,7 @@ const popupContent = [
 // Form Validation
 
 function showModal(ind) {
-  const section = document.querySelectorAll(".card");
+  const section = document.querySelectorAll('.card');
 
   const modalHtml = `
       <div class="modal-container" style="filter: blur(0px);">
@@ -164,18 +164,18 @@ function showModal(ind) {
       </div>
   `;
 
-  section[ind].insertAdjacentHTML("afterbegin", modalHtml);
+  section[ind].insertAdjacentHTML('afterbegin', modalHtml);
 
-  const closeModalIcon = document.querySelector(".x-icon");
-  const modal = document.querySelector(".modal-container");
+  const closeModalIcon = document.querySelector('.x-icon');
+  const modal = document.querySelector('.modal-container');
 
-  closeModalIcon.addEventListener("click", () => {
+  closeModalIcon.addEventListener('click', () => {
     modal.remove();
   });
 }
 
-document.querySelectorAll(".sp-button").forEach((btn, ind) => {
-  btn.addEventListener("click", () => {
+document.querySelectorAll('.sp-button').forEach((btn, ind) => {
+  btn.addEventListener('click', () => {
     showModal(ind);
   });
 });
@@ -218,22 +218,21 @@ document.querySelectorAll('.sp-button').forEach((btn, ind) => {
   });
 });
 
-
 // CONTACT FORM VALIDATION
 const form = document.forms[0];
-const email = form.elements["email"];
-const msg = document.querySelector("small");
+const { email } = form.elements;
+const msg = document.querySelector('small');
 
 function showMsg() {
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (email.value.toLowerCase() !== email.value) {
-      msg.textContent = "E-mail should be in LOWER CASE, Form NOT submitted";
-      msg.classList.add("show");
+      msg.textContent = 'E-mail should be in LOWER CASE, Form NOT submitted';
+      msg.classList.add('show');
     } else {
       form.submit();
     }
-  });  
+  });
 }
 
 showMsg();
