d423a2586ab7d67a64ea27a5c2a081b6472cb0ea
Jordan <camiloarmijosantifonamusic@gmail.com>
2021-12-17 23:13:33 -0500

    Fix linters errors

--- main.js
+++ main.js
@@ -1,123 +1,123 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector("#top-menu");
-const closeIcon = document.querySelector(".close-icon");
-const links = document.querySelectorAll("#top-menu ul li");
-const blurIntro = document.querySelector("#intro");
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('#top-menu');
+const closeIcon = document.querySelector('.close-icon');
+const links = document.querySelectorAll('#top-menu ul li');
+const blurIntro = document.querySelector('#intro');
 
 function display() {
-  mobileMenu.style.transform = "translateX(0%)";
-  blurIntro.style.filter = "blur(3px)";
-  menuIcon.style.display = "none";
+  mobileMenu.style.transform = 'translateX(0%)';
+  blurIntro.style.filter = 'blur(3px)';
+  menuIcon.style.display = 'none';
 }
 
 function disapear() {
-  mobileMenu.style.transform = "translateX(-100%)";
-  blurIntro.style.filter = "blur(0px)";
-  menuIcon.style.display = "inline-block";
+  mobileMenu.style.transform = 'translateX(-100%)';
+  blurIntro.style.filter = 'blur(0px)';
+  menuIcon.style.display = 'inline-block';
 }
 
-menuIcon.addEventListener("click", display);
-closeIcon.addEventListener("click", disapear);
+menuIcon.addEventListener('click', display);
+closeIcon.addEventListener('click', disapear);
 
 links.forEach((link) => {
-  link.addEventListener("click", disapear);
+  link.addEventListener('click', disapear);
 });
 
 // Popup Detail Window and Section 2 Content
 const popupContent = [
   {
-    titleProject: "Tonic",
-    features: ["canopy", "BackEndDev", "2015"],
-    imgProject: "images/cardsWorksSection/card1Detail.svg",
+    titleProject: 'Tonic',
+    features: ['canopy', 'BackEndDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card1Detail.svg',
     contentProject:
       ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      "A daily selection of privately personalized reads; no accounts or sign-ups required."],
-    technologies: ["html", "css", "javaScript", "github", "ruby", "Bootstraps"],
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio"
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg", // x-Icon
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg"
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
-      "See Project",
-      "See&nbsp;live",
-      "See&nbsp;Source"
-    ]
+      'See Project',
+      'See&nbsp;live',
+      'See&nbsp;Source',
+    ],
   },
   {
-    titleProject: "Multi-Post Stories",
-    features: ["facebook", "FullStackDev", "2015"],
-    imgProject: "images/cardsWorksSection/card2.svg",
+    titleProject: 'Multi-Post Stories',
+    features: ['facebook', 'FullStackDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card2.svg',
     contentProject:
       ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      "A daily selection of privately personalized reads; no accounts or sign-ups required."],
-    technologies: ["html", "css", "javaScript", "github", "ruby", "Bootstraps"],
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio"
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg", // x-Icon
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg"
+      'images/icons/exit-icon-gray.svg', // x-Icon
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
-      "See Project"
-    ]
+      'See Project',
+    ],
   },
   {
-    titleProject: "Facebook 360",
-    features: ["canopy", "FullStackDev", "2015"],
-    imgProject: "images/cardsWorksSection/card3.svg",
+    titleProject: 'Facebook 360',
+    features: ['canopy', 'FullStackDev', '2015'],
+    imgProject: 'images/cardsWorksSection/card3.svg',
     contentProject:
     ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "A daily selection of privately personalized reads; no accounts or sign-ups required."],
-    technologies: ["html", "css", "javaScript", "github", "ruby", "Bootstraps"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio"
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg",
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg"
+      'images/icons/exit-icon-gray.svg',
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
-      "See Project"
-    ]
+      'See Project',
+    ],
   },
   {
-    titleProject: "Uber Navigation",
-    features: ["Uber", "LeadDeveloper", "2018"],
-    imgProject: "images/cardsWorksSection/card4.svg",
+    titleProject: 'Uber Navigation',
+    features: ['Uber', 'LeadDeveloper', '2018'],
+    imgProject: 'images/cardsWorksSection/card4.svg',
     contentProject:
     ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      "A daily selection of privately personalized reads; no accounts or sign-ups required."],  
-    technologies: ["html", "css", "javaScript", "github", "ruby", "Bootstraps"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     links: [
-      "https://jord4n-ac.github.io/my-portfolio/#section-2",
-      "https://github.com/Jord4N-AC/my-portfolio"
+      'https://jord4n-ac.github.io/my-portfolio/#section-2',
+      'https://github.com/Jord4N-AC/my-portfolio',
     ],
     icons: [
-      "images/icons/exit-icon-gray.svg",
-      "images/cardsWorksSection/see_live_btn.svg",
-      "images/cardsWorksSection/github_btn.svg"
+      'images/icons/exit-icon-gray.svg',
+      'images/cardsWorksSection/see_live_btn.svg',
+      'images/cardsWorksSection/github_btn.svg',
     ],
     btnText: [
-      "See Project"
-    ]
-  }
+      'See Project',
+    ],
+  },
 ];
 
 // Form Validation
 
 function showModal(ind) {
-  const section = document.querySelectorAll(".card");  
-  
+  const section = document.querySelectorAll('.card');
+
   const modalHtml = `
       <div class="modal-container" style="filter: blur(0px);">
           <div class="header-container">
@@ -163,19 +163,19 @@ function showModal(ind) {
           </div>
       </div>
   `;
-  
-  section[ind].insertAdjacentHTML("afterbegin", modalHtml);
-  
-  const closeModalIcon = document.querySelector(".x-icon");
-  const modal = document.querySelector(".modal-container");
-  
-  closeModalIcon.addEventListener("click", () => {
-    modal.remove();    
+
+  section[ind].insertAdjacentHTML('afterbegin', modalHtml);
+
+  const closeModalIcon = document.querySelector('.x-icon');
+  const modal = document.querySelector('.modal-container');
+
+  closeModalIcon.addEventListener('click', () => {
+    modal.remove();
   });
 }
 
 function createCard() {
-  const cardSection = document.querySelector("#section-2");
+  const cardSection = document.querySelector('#section-2');
   for (let i = popupContent.length - 1; i >= 0; i -= 1) {
     const card = `
     <div id="card${i + 1}" class="card card${i + 1}">
@@ -200,14 +200,14 @@ function createCard() {
   </div>
     `;
 
-    cardSection.insertAdjacentHTML("afterbegin", card);
+    cardSection.insertAdjacentHTML('afterbegin', card);
   }
 }
 
 createCard();
 
-document.querySelectorAll(".sp-button").forEach((btn, ind) => {
-  btn.addEventListener("click", () => {
+document.querySelectorAll('.sp-button').forEach((btn, ind) => {
+  btn.addEventListener('click', () => {
     showModal(ind);
   });
 });
